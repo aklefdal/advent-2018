8d700895c864e494d1fca75238bdb98f0776b193
Alf Kåre Lefdal <alf.kare@lefdal.cc>
2018-12-03 01:35:19 +0100

    AoC dag 2 part 2

--- AdventOfCode/AoC-02.fsx
+++ AdventOfCode/AoC-02.fsx
@@ -43,3 +43,52 @@ Path.Combine(__SOURCE_DIRECTORY__, "AoC-02-input.txt")
 |> readLines
 |> Seq.map parseString
 |> checksum
+
+// Part 2
+
+let compareCharArrays (cs1:char []) (cs2:char []) =
+    let similarChars =
+        cs1
+        |> Array.zip cs2
+        |> Array.filter (fun (c1,c2) -> c1 = c2)
+        
+    if similarChars.Length = cs1.Length - 1 then
+        similarChars
+        |> Array.map fst
+        |> (fun cs -> System.String.Concat(cs))
+        |> Some
+    else
+        None    
+
+let rec findMatch (e:char []) (s: char [] list) =
+    match s with
+    | [] -> None
+    | h::t ->
+        match compareCharArrays e h with
+        | Some solution -> Some solution
+        | None -> findMatch e t 
+
+let rec solve (s: char [] list) =
+    match s with
+    | [] -> None
+    | h::t ->
+        match findMatch h t with
+        | Some solution -> Some solution
+        | None -> solve t    
+
+let testResult2 =
+    [ "abcde"
+      "fghij"
+      "klmno"
+      "pqrst"
+      "fguij"
+      "axcye"
+      "wvxyz" ]
+    |> List.map parseString
+    |> solve
+
+Path.Combine(__SOURCE_DIRECTORY__, "AoC-02-input.txt")
+|> File.ReadAllLines
+|> Array.map parseString
+|> Array.toList
+|> solve
